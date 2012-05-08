b52b76a30a9659f7b87bef3fed45eea7508fd4b8
Donis <donisa+github@gmail.com>
2012-05-08 10:52:32 +0300

    Cleanup

--- transfer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git filter-branch --env-filter '
-
-export GIT_AUTHOR_NAME="Donis"
-export GIT_AUTHOR_EMAIL="donisa+github@gmail.com"
-export GIT_COMMITTER_NAME="Donis"
-export GIT_COMMITTER_EMAIL="donisa+github@gmail.com"
-'
